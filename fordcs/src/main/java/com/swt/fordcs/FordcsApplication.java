8239c921726615c3cddb836a1629e5b64714caf2
karacankos <karacankos@gmail.com>
2022-03-17 22:37:25 +0100

    INI-2394: init commit

--- fordcs/src/main/java/com/swt/fordcs/FordcsApplication.java
+++ fordcs/src/main/java/com/swt/fordcs/FordcsApplication.java
@@ -3,6 +3,7 @@ package com.swt.fordcs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class FordcsApplication {
 
