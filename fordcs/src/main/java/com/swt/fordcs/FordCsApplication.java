88532c05b9a417a3f33a1ba0f6fcf72b8a536440
karacankos <karacankos@gmail.com>
2022-03-24 16:54:39 +0100

    renamed app files

--- fordcs/src/main/java/com/swt/fordcs/FordCsApplication.java
+++ fordcs/src/main/java/com/swt/fordcs/FordCsApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //
 @SpringBootApplication
-public class FordcsApplication {
+public class FordCsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FordcsApplication.class, args);
+		SpringApplication.run(FordCsApplication.class, args);
 	}
 
 }
